1642b9822c8e7afb04e02688d34d49ce559b7187
NathanHolt <nateofakin@gmail.com>
2023-04-16 16:40:59 -0400

    about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,66 @@
 import React from 'react'
+import { Tilt } from 'react-tilt'
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import { services } from '../constants'
+import { fadeIn, textVariant } from '../utils/motion'
+import { SectionWrapper } from '../hoc'
 
-const About = () => {
+const ServiceCard = ({ index, title, icon }) => {
   return (
-    <div>About</div>
+    <Tilt className='xs:w-[250px] w-full'>
+      <motion.div
+        variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450
+          }}
+          className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        >
+          <img 
+            src={icon} 
+            alt={title} 
+            className='w-16 h-16 object-contain' 
+          />
+          <h3 className='text-white text-[20px] font-bold text-center'>
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
   )
 }
 
-export default About
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>
+          Introduction
+        </p>
+        <h2 className={styles.sectionHeadText}>
+          Overview
+        </h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        I'm baby yr labore ea, fugiat polaroid reprehenderit pariatur. Quinoa ramps gastropub, aesthetic cloud bread hell of typewriter elit enamel pin qui austin in sed cred truffaut. Consectetur disrupt put a bird on it, church-key eu pabst YOLO solarpunk cornhole chambray jean shorts tumeric skateboard cloud bread. Mixtape fashion axe poutine you probably haven't heard of them cardigan gentrify ad quinoa labore swag. Leggings dolore VHS irony, cillum gastropub lo-fi disrupt freegan. 
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default SectionWrapper(About, "about");
