605fba48319fed2a19e77a0c195f3a111df22384
NathanHolt <nateofakin@gmail.com>
2023-04-17 16:45:00 -0400

    small change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915eff]">Nathan</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className="sm:block hidden" /> interfaces and web applications
+            I develop web applications,<br className="sm:block hidden" />user interfaces  and 3D visuals 
           </p>
         </div>
       </div>
