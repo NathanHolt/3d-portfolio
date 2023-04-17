5e9e07c6c08d44c094f928b9a126cdc04478be98
NathanHolt <nateofakin@gmail.com>
2023-04-17 17:41:21 -0400

    email working

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,9 +15,44 @@ const Contact = () => {
     message: '',
   })
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const { name, value } = e.target
+
+    setForm({ ...form, [name]: value })
+  }
   
-  const handleSubmit = (e) => {}
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setLoading(true)
+
+    emailjs.send(
+      'service_bkuf4qm',
+      'template_7gjzbqg',
+      {
+        from_name: form.name,
+        to_name: 'Nathan',
+        from_email: form.email,
+        to_email: 'nateofakin@gmail.com',
+        message: form.message,
+      },
+      'V2cQo926VPQtuwDf_'
+    )
+    .then(() => {
+      setLoading(false)
+      alert('Thank you! I will get back to you ASAP!')
+
+      setForm({
+        name: '',
+        email: '',
+        message: '',
+      })
+      
+    }, (error) => {
+      setLoading(false)
+      console.log(error)
+      alert('Something went wrong')
+    })
+  }
 
   return (
     <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
